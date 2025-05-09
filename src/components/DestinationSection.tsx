66c8b65599c8cc54faa729ca8f1e578da0dd43fe
Samuel Santos <42481964-vendasamdora@users.noreply.replit.com>
2025-05-09 18:19:19 +0000

    Restored to 'bae302b97e84fa7c366a9aa74cd00b18b7e226c2'
    Replit-Restored-To: bae302b97e84fa7c366a9aa74cd00b18b7e226c2

--- src/components/DestinationSection.tsx
+++ src/components/DestinationSection.tsx
@@ -35,8 +35,8 @@ const destinations: Destination[] = [
     cityCount: 2
   },
   {
-    id: 'uae',
-    name: 'Emirados Árabes Unidos',
+    id: 'dubai',
+    name: 'Dubai',
     imageSrc: 'https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     cityCount: 1
   },
